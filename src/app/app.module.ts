c7e14262002538580b07cfa41526235c485a4406
Marco <marco.beer93@web.de>
2022-07-24 20:00:04 +0200

    started to design header, court and basket section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CourtsComponent } from './courts/courts.component';
+import { BasketComponent } from './basket/basket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CourtsComponent,
+    BasketComponent
   ],
   imports: [
     BrowserModule,
